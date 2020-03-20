a8b0ac0b2957bd602103dc0fe5aafaa403d7b531
emryan1 <emryan1@vt.edu>
2020-03-20 17:50:23 +0000

    updated routing

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -3,9 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {ViewTicketsComponent} from './view-tickets/view-tickets.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import {SellTicketsComponent} from './sell-tickets/sell-tickets.component';
+import { BuyTicketsComponent } from './buy-tickets/buy-tickets.component';
 import {AuthGuard} from './auth.guard'
 
+
 const routes: Routes = [{path: 'view-tickets', component: ViewTicketsComponent, canActivate: [AuthGuard]},
+{path: 'buy-tickets', component: BuyTicketsComponent, canActivate: [AuthGuard]},
+{path: 'sell-tickets', component: SellTicketsComponent, canActivate: [AuthGuard]},
 {path: '', component: HomeComponent, canActivate: [AuthGuard]}, {path: 'login', component:LoginComponent},
 {path:'**', component:HomeComponent}];
 
