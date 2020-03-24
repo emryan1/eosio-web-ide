312807f78a7baf9db9192e76d328aec5b00bb9b8
emryan1 <emryan1@vt.edu>
2020-03-24 01:10:42 +0000

    added filter for tickets on buy page

--- myapp/src/app/buy-tickets/buy-tickets.component.ts
+++ myapp/src/app/buy-tickets/buy-tickets.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../_services/user.service';
 import { PARecord } from '../_models/PARecord';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-buy-tickets',
@@ -10,13 +11,44 @@ import { PARecord } from '../_models/PARecord';
 export class BuyTicketsComponent implements OnInit {
 
   tickets: PARecord[] = [];
+  tix = [{
+      GameName: 'Football: VT vs. Miami',
+      StadiumSection: "NORTH END ZONE",
+      SportSeason: "Football 2019",
+      Location: "Lane Statium Worsham",
+      GameDate: new Date(),
+      Section: 15,
+      Row: 22,
+      Seat: 18
+    },
+      {
+        GameName: 'Football: VT vs. Miami',
+        StadiumSection: "NORTH END ZONE",
+        SportSeason: "Football 2019",
+        Location: "Lane Statium Worsham",
+        GameDate: new Date(),
+        Section: 15,
+        Row: 22,
+        Seat: 18
+      }];
+  displayedColumns: string[] = ["GameName", "Section", "SportSeason"]
+  dataSource: MatTableDataSource<PARecord>;
+  dataSource2 = new MatTableDataSource(this.tix);
 
-  constructor(private userService:UserService) { }
-
-  ngOnInit() {
+  constructor(private userService:UserService) {
     this.userService.getActivities().subscribe(
       tickets => {this.tickets = tickets}
       );
+      this.dataSource = new MatTableDataSource(this.tickets);
+  }
+
+  ngOnInit() {
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
   }
 
 }
