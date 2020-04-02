4d923af592679a681df78793898ef5270e2d4056
dawway <davidd3@vt.edu>
2020-04-02 22:03:56 +0000

    auction functionality implemented

--- contract/hokietok/hokietok.cpp
+++ contract/hokietok/hokietok.cpp
@@ -50,13 +50,33 @@ CONTRACT hokietok : public eosio::contract {
         uint64_t by_ticket() const { return ticket_id; }
     };
 
-    typedef eosio::multi_index< name("listings"), listing,
+        typedef eosio::multi_index< name("listings"), listing,
             eosio::indexed_by<
                 "ticket"_n, eosio::const_mem_fun<listing, uint64_t, &listing::by_ticket>
             >
         > listing_table;
 
     listing_table listings;
+
+
+        TABLE auction {
+        uint64_t    id;         //unique primary key
+        uint64_t    ticket_id;  //ticket id
+        uint64_t    price;      //price in HOK tokens
+        name        highest_bidder;
+
+        uint64_t primary_key() const { return id; }
+        uint64_t by_ticket() const { return ticket_id; }
+    };
+
+        typedef eosio::multi_index< name("auction"), auction,
+            eosio::indexed_by<
+                "ticket"_n, eosio::const_mem_fun<auction, uint64_t, &auction::by_ticket>
+            >
+        > auction_table;
+
+    auction_table auction_listings;
+
 
   public:
     using contract::contract;
@@ -65,7 +85,8 @@ CONTRACT hokietok : public eosio::contract {
     hokietok( name receiver, name code, datastream<const char*> ds ):
                 contract( receiver, code, ds ),
                 tickets( receiver, receiver.value ),
-                listings( receiver, receiver.value )
+                listings( receiver, receiver.value ),
+                auction_listings(receiver, receiver.value)
     {
         //TODO reconsider scope
     }
@@ -162,4 +183,78 @@ CONTRACT hokietok : public eosio::contract {
         
         listings.erase(lst);
     }
+
+
+    ACTION postauctlst(const uint64_t ticket_id, const uint64_t price) {
+        auto ticket_itr = tickets.find(ticket_id);
+        check(ticket_itr != tickets.end(), "Ticket not found");
+        const auto& ticket = *ticket_itr;
+
+        require_auth(get_self());
+
+        // alternatively, store "for sale" status in ticket
+        auto ticket_index = auction_listings.get_index<"ticket"_n>();
+        check(ticket_index.find(ticket_id) == ticket_index.end(), "Ticket already for sale");
+
+        auction_listings.emplace(get_self(), [&](auto& auction) {
+            auction.id = auction_listings.available_primary_key();
+            auction.ticket_id = ticket.id;
+            auction.price = price;
+            auction.highest_bidder = get_self();
+        });
+    }
+
+    ACTION bidlst(name bidder, const uint64_t bid, const uint64_t auction_id) {
+       require_auth(bidder);
+        auto lst_itr = auction_listings.find(auction_id);
+        check(lst_itr != auction_listings.end(), "Listing not found");
+        const auto& lst = *lst_itr;
+        uint64_t ticket_id = lst.ticket_id;
+        const auto& ticket = tickets.get(ticket_id);
+
+        check(lst.price < bid, "Insuficient Bid");
+
+        //TODO this cast is dangerous
+        auto curr_bid = asset{(int64_t)lst.price, {"HOK", 0}};
+
+        action(
+            permission_level{bidder, "active"_n},
+            "tokenacc"_n,
+            "transfer"_n,
+            std::make_tuple(bidder, lst.highest_bidder, curr_bid, std::string("returning money from displaced bid"))
+        ).send();
+
+        //TODO this cast is dangerous
+        auto new_bid = asset{(int64_t)bid, {"HOK", 0}};
+        action(
+            permission_level{bidder, "active"_n},
+            "tokenacc"_n,
+            "transfer"_n,
+            std::make_tuple(bidder, get_self(), new_bid, std::string("place new bid"))
+
+        ).send();
+        
+        auction_listings.modify(lst, get_self(), [&](auto& t) {
+            t.price = bid;
+            t.highest_bidder = bidder;
+        });
+    }
+
+
+     ACTION closeauclst(const uint64_t listing_id) {
+        require_auth(get_self());
+        auto lst_itr = auction_listings.find(listing_id);
+        check(lst_itr != auction_listings.end(), "Listing not found");
+        const auto& lst = *lst_itr;
+        uint64_t ticket_id = lst.ticket_id;
+        const auto& ticket = tickets.get(ticket_id);
+
+        tickets.modify(ticket, get_self(), [&] (auto& t) {
+            t.owner = lst.highest_bidder;
+        });
+        
+        
+        auction_listings.erase(lst);
+    }
+
 };
