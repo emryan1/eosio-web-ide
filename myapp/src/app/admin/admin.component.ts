1ae8a54a7a55a22e0414b91c4a172a0862d83b72
Evan Jones <jevan@vt.edu>
2020-04-06 23:41:35 +0000

    Admin page fully functional

--- myapp/src/app/admin/admin.component.ts
+++ myapp/src/app/admin/admin.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {PARecord} from '../_models/PARecord'
+import {FormControl} from '@angular/forms';
+import { EosApiService } from '../_services/eos-api.service';
+import {NotificationService} from '../_services/notification.service'
+
 
 @Component({
   selector: 'app-admin',
@@ -8,51 +12,91 @@ import {PARecord} from '../_models/PARecord'
 })
 export class AdminComponent implements OnInit {
 
-  private records: PARecord[];
-  private record: PARecord;
-  private numTix = 1;
+  ticket: PARecord;
+  numTix = 1;
+  date = new FormControl(new Date())
 
-  constructor() { }
+  constructor(private api:EosApiService, private notif:NotificationService) { }
 
   ngOnInit() {
+    this.ticket = new PARecord();
+    this.ticket.game = "Football: VT vs. Miami";
+    this.ticket.stadium_section = "NORTH END ZONE";
+    this.ticket.season = "Football 2019";
+    this.ticket.location = "Lane Statium Worsham";
+    this.ticket.section = 15;
+    this.ticket.row = 22;
+    this.ticket.seat = 18;
   }
 
   temp(event: Event) {
-      const filterValue = (event.target as HTMLInputElement).value;
+    const filterValue = (event.target as HTMLInputElement).value;
   }
 
   setGame(event: Event) {
-      const value = (event.target as HTMLInputElement).value;
-      this.record.game = value;
+    const value = (event.target as HTMLInputElement).value;
+    this.ticket.game = value;
   }
 
   setStadiumSection(event: Event) {
-      const value = (event.target as HTMLInputElement).value;
-      this.record.stadium_section = value;
+    const value = (event.target as HTMLInputElement).value;
+    this.ticket.stadium_section = value;
   }
 
   setSeason(event: Event) {
-      const value = (event.target as HTMLInputElement).value;
-      this.record.season = value;
+    const value = (event.target as HTMLInputElement).value;
+    this.ticket.season = value;
   }
 
   setLocation(event: Event) {
-      const value = (event.target as HTMLInputElement).value;
-      this.record.location = value;
+    const value = (event.target as HTMLInputElement).value;
+    this.ticket.location = value;
   }
 
   setDate(event: Event) {
-      const value = (event.target as HTMLInputElement).value;
-      this.record.date = new Date(value);
+    const value = (event.target as HTMLInputElement).value;
+    this.ticket.date = new Date(value);
   }
 
   setSection(event: Event) {
-      const value = (event.target as HTMLInputElement).value;
-      this.record.section = parseInt(value);
+    const value = (event.target as HTMLInputElement).value;
+    this.ticket.section = parseInt(value);
   }
 
   setRow(event: Event) {
-      const value = (event.target as HTMLInputElement).value;
-      this.record.row = parseInt(value);
+    const value = (event.target as HTMLInputElement).value;
+    this.ticket.row = parseInt(value);
+  }
+
+  setSeat(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.ticket.seat = parseInt(value);
+  }
+
+  setNumber(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.numTix = parseInt(value);
+  }
+
+  submitSingle() {
+    this.notif.showNotif("Submitting", 'ok');
+    this.ticket.date = this.date.value;
+    this.api.createTicket(this.ticket).subscribe( () => {this.notif.showNotif("Submitted", 'ok');},
+      () => {
+        this.notif.showNotif("Error submitting", 'ok');
+    });
+  }
+
+  submitMultiple() {
+    this.notif.showNotif("Submitting", 'ok');
+    this.ticket.date = this.date.value;
+    let i = 0;
+    for(i = 0; i < this.numTix; i++) {
+      this.ticket.seat = i + 1;
+      this.api.createTicket(this.ticket).subscribe( () => {this.notif.showNotif("Submitted", 'ok');},
+        () => {
+          this.notif.showNotif("Error submitting", 'ok');
+      });
+    }
   }
 }
