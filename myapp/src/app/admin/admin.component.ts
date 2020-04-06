737e0ef489694ef1aa2faf09f73cb649572c3619
Evan Jones <jevan@vt.edu>
2020-04-06 21:32:15 +0000

    Started more admin page

--- myapp/src/app/admin/admin.component.ts
+++ myapp/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PARecord} from '../_models/PARecord'
 
 @Component({
   selector: 'app-admin',
@@ -7,12 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  private records: PARecord[];
+  private record: PARecord;
+  private numTix = 1;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   temp(event: Event) {
-      return;
+      const filterValue = (event.target as HTMLInputElement).value;
+  }
+
+  setGame(event: Event) {
+      const value = (event.target as HTMLInputElement).value;
+      this.record.game = value;
+  }
+
+  setStadiumSection(event: Event) {
+      const value = (event.target as HTMLInputElement).value;
+      this.record.stadium_section = value;
+  }
+
+  setSeason(event: Event) {
+      const value = (event.target as HTMLInputElement).value;
+      this.record.season = value;
+  }
+
+  setLocation(event: Event) {
+      const value = (event.target as HTMLInputElement).value;
+      this.record.location = value;
+  }
+
+  setDate(event: Event) {
+      const value = (event.target as HTMLInputElement).value;
+      this.record.date = new Date(value);
+  }
+
+  setSection(event: Event) {
+      const value = (event.target as HTMLInputElement).value;
+      this.record.section = parseInt(value);
+  }
+
+  setRow(event: Event) {
+      const value = (event.target as HTMLInputElement).value;
+      this.record.row = parseInt(value);
   }
 }
